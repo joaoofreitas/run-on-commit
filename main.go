7776f467e1d8106dab0da3e8de0c17f0d195eff6
joaoofreitas <joaoofreitas00@gmail.com>
2020-10-05 22:59:44 +0100

    SHA file added as memory, content reading and conditionals done.

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
     "io/ioutil"
     "encoding/json"
 )
-func main() {
+
+func getCommitSHA(url string) string{ 
     type CommitSHA struct {
 	SHA string `json:"sha"`
     }
@@ -14,21 +15,35 @@ func main() {
 	Content CommitSHA `json:"object"`
     }
 
-    res, _ := http.Get("http://api.github.com/repos/joaoofreitas/vue-portfolio/git/refs/heads/master") 
+    res, _ := http.Get(url) 
 
     responseInBytes, _ := ioutil.ReadAll(res.Body)
-    //responseInStr := string(responseInBytes)
 
     var responseObject Response
     json.Unmarshal(responseInBytes, &responseObject)
 
     fmt.Println(responseObject.Content.SHA)
+    return string(responseObject.Content.SHA)
+}
+func main() {
+
+    file, _ := ioutil.ReadFile("sha")
+    commit := getCommitSHA("http://api.github.com/repos/joaoofreitas/vue-portfolio/git/refs/heads/master")
+
+    if string(file) != commit{
+	// Run Update Function
+	fmt.Println("There are some changes, updating containers...")
+	ioutil.WriteFile("sha", []byte(commit), 0644)
+    } else {
+	fmt.Println("Nothing changed...Exiting")
+    }
+
 }
 
 /*
 Todo: 
-    Store ---> responseObject.Content.SHA in a file.
-    @START ---> read content in the file and perform the request
+    Store ---> responseObject.Content.SHA in a file. DONE
+    @START ---> read content in the file and perform the request DONE
     Add argv for user and repository
     Write the conditions
 */
